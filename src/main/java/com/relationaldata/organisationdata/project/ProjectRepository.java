06f2326abd47e8e5139b1c7839a25c74876df1fc
Yansh Bhardwaj <64511677+Yaambs18@users.noreply.github.com>
2021-12-08 10:31:03 +0530

    Delete ProjectRepository.java

--- src/main/java/com/relationaldata/organisationdata/project/ProjectRepository.java
+++
@@ -1,6 +0,0 @@
-package com.relationaldata.organisationdata.project;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
-}
