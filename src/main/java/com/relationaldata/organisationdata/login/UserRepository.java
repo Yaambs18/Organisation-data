66f76660d0736b8a2feab8a6a05ecf0526aab798
Yansh Bhardwaj <64511677+Yaambs18@users.noreply.github.com>
2021-12-08 10:30:40 +0530

    Delete UserRepository.java

--- src/main/java/com/relationaldata/organisationdata/login/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.relationaldata.organisationdata.login;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-}
